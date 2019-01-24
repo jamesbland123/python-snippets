440d6128591494c9b906c26f4a6ed9658b50c57c
jamesbland123 <james@teclait.com>
2019-01-23 21:19:35 -0800

    Added traditional for loop in addition to dict comprehension

--- dups.py
+++ dups.py
@@ -1,4 +1,8 @@
+""" A an example of using defaultdict to find 
+    duplicates in a list """
+
 from collections import defaultdict
+
 
 fruits = ["apple", "pear", "orange", "apple", "orange"]
 
@@ -11,5 +15,11 @@ for i in fruits:
 dups = {k:v for k,v in count.items() if v >= 2}
 
 print(dups)
+
+# Alternative to dictionary comprehension
+
+for k in list(count):
+    if count[k] >= 2:
+        print("Key: {} has count {}".format(k, count[k]))
 
 
