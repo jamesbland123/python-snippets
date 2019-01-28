97abc741c78a5f8d7fc05cdc7d94335a956fd6d5
jamesbland123 <james@teclait.com>
2019-01-27 19:01:53 -0800

    stair climber recursion problem

---
+++ stair_climber.py
@@ -0,0 +1,46 @@
+""" Stair climber problem 
+There's a staircase with N steps, and you can climb 1 or 2 steps at a time. 
+Given N, write a function that returns the number of unique ways you can climb 
+the staircase. The order of the steps matters.
+
+For example, if N is 4, then there are 5 unique ways:
+
+1, 1, 1, 1
+2, 1, 1
+1, 2, 1
+1, 1, 2
+2, 2
+What if, instead of being able to climb 1 or 2 steps at a time, you could climb 
+any number from a set of positive integers X? For example, if X = {1, 3, 5}, you 
+could climb 1, 3, or 5 steps at a time. Generalize your function to take in X. """
+
+
+def staircase(n):
+    if n <= 1:
+        return 1
+    return staircase(n - 1) + staircase(n - 2)
+
+print(staircase(4))    
+
+def staircase2(n):
+    a, b = 1, 2
+    for _ in range(n - 1):
+        a, b = b, a + b
+    return a
+
+def staircase3(n, X):
+    if n < 0:
+        return 0
+    elif n == 0:
+        return 1
+    else:
+        return sum(staircase3(n - x, X) for x in X)
+
+def staircase4(n, X):
+    cache = [0 for _ in range(n + 1)]
+    cache[0] = 1
+    for i in range(1, n + 1):
+        cache[i] += sum(cache[i - x] for x in X if i - x >= 0)
+    return cache[n]
+
+print(staircase4(4, [1,2]))
